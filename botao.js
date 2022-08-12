18acba8acb84ea3047196e98389a68352ab0135d
Milena Souza <61788510+somilena@users.noreply.github.com>
2022-08-12 13:53:40 -0300

    Add files via upload

---
+++ botao.js
@@ -0,0 +1,24 @@
+function insert(num){
+    document.form.textview.value = document.form.textview.value + num;
+}
+function equal(){
+    exp = document.form.textview.value;
+    if(exp){
+       document.form.textview.value = eval(exp);
+}
+}
+function c(){
+    document.form.textview.value = "";
+}
+function back(){
+    var exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0, exp.length-1);
+}
+
+function mudar_cor (choosecolor)
+{
+if(choosecolor)
+{
+document.getElementById('calcor').style.background=choosecolor;
+}
+}
